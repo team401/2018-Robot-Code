6e7b2cb45f1c628a50cb1dc0ba5cea0f5d59bed9
Cameron Earle <camearle20@me.com>
2018-01-05 16:05:44 -0500

    Added base project files

---
+++ robot/src/main/kotlin/org/team401/robot2018/Setup.kt
@@ -0,0 +1,22 @@
+package org.team401.robot2018
+
+import org.snakeskin.dsl.*
+import org.snakeskin.registry.*
+
+/*
+ * 2018-Robot-Code - Created on 1/5/18
+ * Author: Cameron Earle
+ * 
+ * This code is licensed under the GNU GPL v3
+ * You can find more info in the LICENSE file at project root
+ */
+
+/**
+ * @author Cameron Earle
+ * @version 1/5/18
+ */
+
+@Setup fun setup() {
+    Subsystems.add() //TODO Add subsystems
+    Controllers.add() //TODO Add controllers
+}
---
+++ vision/src/main/kotlin/org/team401/vision2018/Vision.kt
@@ -0,0 +1,18 @@
+package org.team401.vision2018
+
+/*
+ * 2018-Robot-Code - Created on 1/5/18
+ * Author: Cameron Earle
+ * 
+ * This code is licensed under the GNU GPL v3
+ * You can find more info in the LICENSE file at project root
+ */
+
+/**
+ * @author Cameron Earle
+ * @version 1/5/18
+ */
+
+fun main(args: Array<String>) {
+
+}
