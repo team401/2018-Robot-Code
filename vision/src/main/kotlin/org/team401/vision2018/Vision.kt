b71995741262e4bfb2124bdaf9e620ae7fff739c
Cameron Earle <camearle20@me.com>
2018-01-11 09:28:27 -0500

    Added test code for streaming on a pi

--- vision/src/main/kotlin/org/team401/vision2018/Vision.kt
+++ vision/src/main/kotlin/org/team401/vision2018/Vision.kt
@@ -1,5 +1,11 @@
 package org.team401.vision2018
 
+import org.opencv.core.Core
+import org.team401.snakeeyes.Cameras
+import org.team401.snakeeyes.camera.Camera
+import org.team401.snakeeyes.service.MjpegServer
+import org.team401.snakeeyes.view.CameraView
+
 /*
  * 2018-Robot-Code - Created on 1/5/18
  * Author: Cameron Earle
@@ -14,5 +20,14 @@ package org.team401.vision2018
  */
 
 fun main(args: Array<String>) {
+    System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
+    val cam = Camera()
+    cam.open(0)
 
+    Cameras.add(cam)
+    Cameras.start()
+
+    val view = CameraView(cam)
+    val server = MjpegServer(8080, view)
+    server.start()
 }
