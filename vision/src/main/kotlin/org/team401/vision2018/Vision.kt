e84266a8d0d910bd5cd3aa27a44695f316a5d6aa
Cameron Earle <camearle20@me.com>
2018-01-12 08:58:44 -0500

    Added final version of camera streamer code

--- vision/src/main/kotlin/org/team401/vision2018/Vision.kt
+++ vision/src/main/kotlin/org/team401/vision2018/Vision.kt
@@ -5,6 +5,7 @@ import org.team401.snakeeyes.Cameras
 import org.team401.snakeeyes.camera.Camera
 import org.team401.snakeeyes.service.MjpegServer
 import org.team401.snakeeyes.view.CameraView
+import org.team401.snakeeyes.view.PipView
 
 /*
  * 2018-Robot-Code - Created on 1/5/18
@@ -21,13 +22,18 @@ import org.team401.snakeeyes.view.CameraView
 
 fun main(args: Array<String>) {
     System.loadLibrary(Core.NATIVE_LIBRARY_NAME)
-    val cam = Camera()
-    cam.open(0)
 
-    Cameras.add(cam)
+    val topView = Camera()
+    val frontView = Camera()
+
+    topView.open("TOP_PATH")        //TODO add correct paths (/dev/v4l/by-id/...)
+    frontView.open("FRONT_PATH")
+
+    Cameras.add(topView)
+    Cameras.add(frontView)
     Cameras.start()
 
-    val view = CameraView(cam)
-    val server = MjpegServer(8080, view)
+    val view = PipView(CameraView(topView), CameraView(frontView), PipView.Position.TOP_CENTER)
+    val server = MjpegServer(1180, view)
     server.start()
 }
