b373fec38d84c489c4c151b4ccbf29fb6e0c15f9
DarkenedDragon <eridor314@gmail.com>
2018-02-05 17:19:21 -0500

    Added test mode for drivetrain

--- robot/src/main/kotlin/org/team401/robot2018/subsystems/Drivetrain.kt
+++ robot/src/main/kotlin/org/team401/robot2018/subsystems/Drivetrain.kt
@@ -7,6 +7,7 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import com.ctre.phoenix.sensors.PigeonIMU
 import edu.wpi.first.wpilibj.PowerDistributionPanel
 import edu.wpi.first.wpilibj.Solenoid
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import org.snakeskin.ShifterState
 import org.snakeskin.component.Gearbox
 import org.snakeskin.dsl.*
@@ -198,6 +199,100 @@ val DrivetrainSubsystem: Subsystem = buildSubsystem {
                 Drivetrain.shiftUpdate(newState)
             }
         }
+    }
+
+    test("Drivetrain test"){
+        println("Running drivetrain test")
+
+        Thread.sleep(1000)
+        leftFront.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val leftFrontEncPosition = leftFront.getSelectedSensorPosition(0)
+        val leftFrontEncVelocity = leftFront.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("leftFront Position", leftFrontEncPosition.toDouble())
+        SmartDashboard.putNumber("leftFront Velocity", leftFrontEncVelocity.toDouble())
+        leftFront.set(ControlMode.PercentOutput, 0.0)
+
+        Thread.sleep(1000)
+        leftMidF.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val leftMidFEncPosition = leftMidF.getSelectedSensorPosition(0)
+        val leftMidFEncVelocity = leftMidF.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("leftMidF Position", leftMidFEncPosition.toDouble())
+        SmartDashboard.putNumber("leftMidF Velocity", leftMidFEncVelocity.toDouble())
+        leftMidF.set(ControlMode.PercentOutput, 0.0)
+
+        Thread.sleep(1000)
+        leftMidR.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val leftMidREncPosition = leftMidR.getSelectedSensorPosition(0)
+        val leftMidREncVelocity = leftMidR.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("leftMidR Position", leftMidREncPosition.toDouble())
+        SmartDashboard.putNumber("leftMidR Velocity", leftMidREncVelocity.toDouble())
+        leftMidR.set(ControlMode.PercentOutput, 0.0)
+
+        Thread.sleep(1000)
+        leftRear.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val leftRearEncPosition = leftRear.getSelectedSensorPosition(0)
+        val leftRearEncVelocity = leftRear.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("leftRear Position", leftRearEncPosition.toDouble())
+        SmartDashboard.putNumber("leftRear Velocity", leftRearEncVelocity.toDouble())
+        leftRear.set(ControlMode.PercentOutput, 0.0)
+
+        Thread.sleep(1000)
+        rightFront.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val rightFrontEncPosition = rightFront.getSelectedSensorPosition(0)
+        val rightFrontEncVelocity = rightFront.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("rightFront Position", rightFrontEncPosition.toDouble())
+        SmartDashboard.putNumber("rightFront Velocity", rightFrontEncVelocity.toDouble())
+        rightFront.set(ControlMode.PercentOutput, 0.0)
+
+        Thread.sleep(1000)
+        rightMidF.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val rightMidFEncPosition = rightMidF.getSelectedSensorPosition(0)
+        val rightMidFEncVelocity = rightMidF.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("rightMidF Position", rightMidFEncPosition.toDouble())
+        SmartDashboard.putNumber("rightMidF Velocity", rightMidFEncVelocity.toDouble())
+        rightMidF.set(ControlMode.PercentOutput, 0.0)
+
+        Thread.sleep(1000)
+        rightMidR.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val rightMidREncPosition = rightMidR.getSelectedSensorPosition(0)
+        val rightMidREncVelocity = rightMidR.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("rightMidR Position", rightMidREncPosition.toDouble())
+        SmartDashboard.putNumber("rightMidR Velocity", rightMidREncVelocity.toDouble())
+        rightMidR.set(ControlMode.PercentOutput, 0.0)
+
+        Thread.sleep(1000)
+        rightRear.set(ControlMode.PercentOutput, 1.0)
+        Thread.sleep(1000)
+        //Not sure if pidIdx if correct
+        val rightRearEncPosition = rightRear.getSelectedSensorPosition(0)
+        val rightRearEncVelocity = rightRear.getSelectedSensorVelocity(0)
+        SmartDashboard.putNumber("rightRear Position", rightRearEncPosition.toDouble())
+        SmartDashboard.putNumber("rightRear Velocity", rightRearEncVelocity.toDouble())
+        rightRear.set(ControlMode.PercentOutput, 0.0)
+
+        leftMidF.follow(leftFront)
+        leftMidR.follow(leftFront)
+        leftRear.follow(leftFront)
+
+        rightMidF.follow(rightFront)
+        rightMidR.follow(rightFront)
+        rightRear.follow(rightFront)
+
+        false
     }
 
     on (Events.TELEOP_ENABLED) {
