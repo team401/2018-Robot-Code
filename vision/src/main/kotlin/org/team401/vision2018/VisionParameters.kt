97d8bf1d0ac5ea0bdbc4c4e4b810c4bcafa9d0ed
Cameron <camearle20@me.com>
2018-02-06 19:57:29 -0500

    Added camera settings skeleton

--- vision/src/main/kotlin/org/team401/vision2018/VisionParameters.kt
+++ vision/src/main/kotlin/org/team401/vision2018/VisionParameters.kt
@@ -7,7 +7,9 @@ data class VisionParameters(val procRegionStart: List<Int>,
                             val sThresh: List<Double>,
                             val vThresh: List<Double>,
                             val externalContoursOnly: Boolean,
-                            val filtering: FilteringParameters) {
+                            val filtering: FilteringParameters,
+                            val topCamera: CameraSettings,
+                            val frontCamera: CameraSettings) {
 
     data class FilteringParameters(val minArea: Double,
                                    val minPerimeter: Double,
@@ -16,4 +18,6 @@ data class VisionParameters(val procRegionStart: List<Int>,
                                    val solidity: List<Double>,
                                    val vertices: List<Double>,
                                    val ratio: List<Double>)
+
+    data class CameraSettings(val brightness: Int)
 }
